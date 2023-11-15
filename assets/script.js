86858ef86dde44de317459a95a084c07a5dab949
Cyril FERRER <cyril.ferrer31@gmail.com>
2023-11-15 14:23:46 +0100

    gestion BP via index courant, suppression des attributs onclick, fusion des fonctions de sliding

--- assets/script.js
+++ assets/script.js
@@ -18,17 +18,13 @@ const slides = [
 	}
 ]
 
-//compteurs (slider et bullets points)
 let compteur = 0
-let iDot = 1 
-let i2 = 0
 //récupération d'éléments
 const arrowLeft = document.querySelector("#banner .arrow_left")
 const picture = document.querySelector(".banner-img")
 const blurb = document.querySelector("#banner p")
 const arrowRight = document.querySelector("#banner .arrow_right")
 const tableauDot = [] 
-let targetDot = document.querySelector(`.dots :nth-child(${iDot})`)
 let dots = document.querySelector(".dots")
 //récupération des valeurs // à intégrer en fonction
 let completionImage = slides[compteur].image 
@@ -36,80 +32,54 @@ let completionTexte = slides[compteur].tagLine
 // variable pour stock d'élements à créer/modifier
 let dot
 
-//renvoie fonction "show(n)" -- pour l'attr onclick des BP
-function writing(n){
-	let testing = `show(${n})`
-	return testing
-}
 
 //mise en place des BP
 slides.forEach(() =>
 	{dot = document.createElement("div")
 	dots.append(dot)
 	dot.classList.add("dot")
-	dot.setAttribute("onclick", writing(i2))
-	i2 ++
+	dot.addEventListener("click", function(event)
+		{bulletChange(event.target)})
 	tableauDot.push(dot)}
 )
 tableauDot[0].classList.add("dot_selected")
 
-
-
-//actualise bannière selon BP cliqué
-function show(n){
-	eraseDot(iDot)
-	compteur = n
-	iDot = compteur+1
-	actualizeBanner(compteur, iDot)
-}
-
 //Déclenchement des boutons-flèches au clic
 arrowLeft.addEventListener("click", function(event)
-	{sliding(event.clientX)}
+	{sliding(event.target)}
 )
 arrowRight.addEventListener("click", function(event)
-	{sliding(event.clientX)}
+	{sliding(event.target)}
 )
 
-//actualisation bannière selon L/R
-function sliding(clientX){
-	eraseDot(iDot)
-	clientX > 100 ? Right() : Left()
-	actualizeBanner(compteur, iDot)
-}
-function Left(){
-	if(compteur > 0){
-		compteur --
-		iDot --
-	}
-	else{
-		compteur = slides.length-1
-		iDot = slides.length
-	}
-}
-function Right(){
-	if (compteur < slides.length-1){
-		compteur ++
-		iDot ++
-	}
-	else{
-		compteur = 0
-		iDot = 1
-	}
+
+
+
+function bulletChange(evenement){
+	tableauDot[compteur].classList.remove("dot_selected")
+	compteur = tableauDot.indexOf(evenement)
+	actualizeBanner(compteur)
+	tableauDot.forEach(() =>
+		{dot.classList.remove("dot_selected")}
+	)
+	evenement.classList.add("dot_selected")
 }
 
-//récup valeurs puis modification des élements de la bannière
-function actualizeBanner(compteur, iDot){
+function sliding(client){
+	tableauDot[compteur].classList.remove("dot_selected")
+	//client == arrowRight ? Right() : Left()
+	if(client == arrowRight)
+		{compteur < slides.length-1 ? compteur++ : compteur = 0}
+	else
+		{compteur > 0 ? compteur -- : compteur = slides.length-1}
+	actualizeBanner(compteur)
+}
+
+function actualizeBanner(compteur){
 	completionImage = slides[compteur].image
 	completionTexte = slides[compteur].tagLine
 	picture.setAttribute("src", "./assets/images/slideshow/"+completionImage)
 	blurb.innerHTML = completionTexte
-	targetDot = document.querySelector(".dots :nth-child("+iDot+")")
-	targetDot.classList.add("dot_selected")
-}
-//BP actuel déselectionné
-function eraseDot(iDot){
-	dot = document.querySelector(".dots :nth-child("+(iDot)+")")
-	dot.classList.remove("dot_selected")
+	tableauDot[compteur].classList.add("dot_selected")
 }
 
